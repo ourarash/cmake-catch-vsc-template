6e07e974870b9c1baad5300d1a4a4ccee6aad60a
Ari <ourarash@gmail.com>
2021-01-23 11:34:02 -0800

    Fixed abseil problem

--- src/abseil_demo/hello_world.cc
+++ src/abseil_demo/hello_world.cc
@@ -4,6 +4,7 @@
 #include "absl/strings/str_join.h"
 
 int main() {
+  std::cout << "Hellow world for Abseil." << std::endl;
   std::vector<std::string> v = {"foo","bar","baz"};
   std::string s = absl::StrJoin(v, "-");
 
--- src/main/main.cpp
+++ src/main/main.cpp
@@ -8,7 +8,6 @@ int main(int argc, const char *argv[]) {
   CPPLib my_lib;
   auto greeting = my_lib.GetGreetingMessage();
 
-
   std::cout << greeting << std::endl;
   return 0;
 }
